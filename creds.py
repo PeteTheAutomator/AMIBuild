b2c1b9e5aa9c85271c4c17883273d07de01b2493
Peter Hehn <peter.hehn@airwalkconsulting.com>
2018-02-21 14:09:35 +0000

    resolving creds

---
+++ creds.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import requests
+import json
+
+if 'AWS_CONTAINER_CREDENTIALS_RELATIVE_URI' not in os.environ.keys():
+    print("Environment variable AWS_CONTAINER_CREDENTIALS_RELATIVE_URI is not set - aborting...")
+    sys.exit(1)
+
+creds_url="http://169.254.170.2" + os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
+
+r = requests.get(creds_url)
+d = json.loads(r._content)
+
+d = json.loads(example)
+
+creds = "aws_access_key: " + d['AccessKeyId'] + '\n'
+creds += "aws_secret_key: " + d['SecretAccessKey'] + '\n'
+creds += "security_token: " + d['Token']
+
+with open('creds.yml', 'w') as fh:
+    fh.write(creds)
+
+fh.close()
+
