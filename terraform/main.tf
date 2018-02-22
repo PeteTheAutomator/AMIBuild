273cc916e26f46ea7e5cb984fbc1aac22690530e
Peter Hehn <peter.hehn@airwalkconsulting.com>
2018-02-22 12:16:55 +0000

    extended to begin some baking

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,10 @@ module "codebuild" {
     codebuild_source_location = "https://github.com/PeteTheAutomator/AMIBuild.git"
     codebuild_image = "docker.io/petetheautomator/ansible"
 }
+
+resource "aws_iam_policy_attachment" "codebuild_policy_attachment2" {
+  name       = "codebuild-policy-attachment2"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  roles      = ["${module.codebuild.codebuild_role_id}"]
+}
+
